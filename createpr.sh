cc26179d498786db1726d2d2889998a23ffbb057
itkan <gupta.ankit.vi@gmail.com>
2023-12-30 16:20:29 +0530

    pr script

---
+++ createpr.sh
@@ -0,0 +1,6 @@
+git branch -D initial-commit
+git checkout -b initial-commit
+git add .
+git commit -m "initial commit"
+git push --set-upstream origin initial-commit
+open https://github.com/itkan/srjwellers.github.io/pull/new/initial-commit

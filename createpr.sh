289834f2245b7d0e04546a83dbfb88f1162ace58
itkan <gupta.ankit.vi@gmail.com>
2023-12-30 17:04:35 +0530

    initial commit

--- createpr.sh
+++ createpr.sh
@@ -1,6 +1,10 @@
 git branch -D initial-commit
 git checkout -b initial-commit
+rm -rf docs/
+cp -R ../jwelleryShop/jewelry-catalog/build docs/
 git add .
 git commit -m "initial commit"
 git push --set-upstream origin initial-commit
 open https://github.com/itkan/srjwellers.github.io/pull/new/initial-commit
+git checkout main
+git pull
